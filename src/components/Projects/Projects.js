8203c9803fefa20f8c90c103c567189d7460b5a1
Dmitry Goronkov <gordima@yahoo.com>
2019-06-17 11:50:57 -0400

    all pages are done. Need media adjustments

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,13 +18,13 @@ export default class Projects extends React.Component{
         isHidden2: false,
         light: false
     }
+    onClickDetails =()=>{
+        this.setState({isHidden2:true})
+    }
     componentDidMount(){
         setInterval(()=> {
             this.setState({ isHidden: true})
         }, 5000);
-        setInterval(()=> {
-            this.setState({ isHidden2: true})
-        }, 10000);
         this.setState({light: true})
     }
     render(){
@@ -47,8 +47,21 @@ export default class Projects extends React.Component{
 	            <div class="bird--wrap bird--wrap--four">
 		            <div class="bird bird--four"></div>
 	            </div>
-                <div className="appvideo" ><iframe width="320" height="480" src="https://www.youtube.com/embed/OZMa_dqRO4c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                </div>
+                <Icons className="backPro" pose={this.state.isHidden? "finish":"start"}>
+                    <Link className="buttonProBack--link" to="/"><button className="buttonProBack">Back</button></Link>
+                </Icons> 
+                <Icons className="beagiver" pose={this.state.isHidden? "finish":"start"}>
+                    <div className="appvideo" >
+                        <iframe width="320" height="480" src="https://www.youtube.com/embed/IeIJ3_X8GBc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                    </div>
+                    <button onClick={this.onClickDetails} className="buttonPro">Details</button>
+                </Icons>
+                <Icons className="beagiver--description" pose={this.state.isHidden2? "finish":"start"}>
+                    <div className="beagiver--description__text">
+                        <p> <span style={{fontWeight:"bold"}}>BeAGiver</span> is a mobile app with the goal to reduce food waste. Over <span style={{fontWeight:"bold"}}> $31 billion </span> dollars worth of food is wasted in Canada each year. This is enough to feed  <span style={{fontWeight:"bold"}}>every Canadian for five month. </span> Tons of food are thrown out every night by grocery stores and restaurants due to not matching the organization’s high standards. This food could still be used elsewhere for consumption, in fact it is estimated that approximately <span style={{fontWeight:"bold"}}> 80% </span> of consumer food was once perfectly edible. BeAGiver attempts to solve this problem. The app connects charity organizations with grocery stores and restaurants and helps organize pick up of their extra food for further distribution to homeless and people in need.</p>
+                    </div>
+                </Icons>
+                
             </div>
         )
     }
