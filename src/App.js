fa2e35570627c8460a797c9191b73df4b93e5cf6
Dmitry Goronkov <gordima@yahoo.com>
2019-07-08 14:08:26 -0400

    Website restructured completely

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import './App.scss';
+// import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import posed, { PoseGroup } from 'react-pose';
 import Main from './components/Main/Main'
 import Profile from './components/Profile/Profile'
 import Projects from './components/Projects/Projects'
 import ReactGA from 'react-ga';
+import technology from './assets/svg/innovation.svg'
+import colaboration from './assets/svg/hands.svg'
+import ps from './assets/svg/problem-solving.svg'
+import love from './assets/svg/care.svg'
+import github from './assets/svg/github-logo.svg'
+import linkedin from './assets/svg/linkedin-logo.svg'
+import curly from './assets/svg/curly-hair.svg'
+import wordpress from './assets/svg/wordpress.svg'
+import mockup from './assets/mockup.png'
 function initializeReactGA() {
   ReactGA.initialize('UA-142705554-1');
   ReactGA.pageview('/homepage');
@@ -19,13 +28,53 @@ class App extends React.Component {
   render(){
   return (
     <div>
-       <BrowserRouter>
+       {/* <BrowserRouter>
           <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/profile" component={Profile} />
             <Route path="/projects" component={Projects} />
           </Switch>
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <Main></Main>
+        <div className="projects">
+          <div className="projects__icons">
+            <div className="projects__icons__technology">
+              <img src={technology} alt="technology"/>
+              <div className="label">I am proficient in most up-to-date technologies like <span>React</span>, <span>Redux</span>, <span>Express</span> and <span>cloud</span> databases</div>
+            </div>
+            <div className="projects__icons__colaboration">
+              <img src={colaboration} alt="colaboration"/>
+              <div className="label">I <span>collaborate</span> effectively to get things done, building and nurturing strong relationships</div>
+            </div>
+            <div className="projects__icons__ps">
+              <img src={ps} alt="problem-solving"/>
+              <div className="label">I embrace <span>challenges</span> and implement the most viable <span>solutions</span></div>
+            </div>
+            <div className="projects__icons__love">
+              <img src={love} alt="done with love"/>
+              <div className="label">Everything that I do is done with <span>love</span> and <span>attention</span></div>
+            </div>
+          </div>
+          <div className="projects__intro">
+            <img className="projects__intro__logo" src={curly} alt="logo"/>
+            <div className="projects__intro__greeting">
+              <span>Hi! My name is Dmitry Goronkov. I am a web developer from Toronto, Canada. I am also an author of the blog <span className="bold"><a href="https://webdevsnippets.home.blog/">Web Development Snippets. </a></span> My personal motto: "I live to be corrected."</span>
+              <div className="projects__intro__greeting__links">
+                <a href="https://www.linkedin.com/in/dmitry-goronkov/"><img src={linkedin} alt="linkedin"/></a>
+                <a href="https://github.com/DmitryGoronkov"><img src={github} alt="github"/></a>
+                <a href="https://webdevsnippets.home.blog/"><img src={wordpress} alt="blog"/></a>
+              </div>
+            </div> 
+            <div className="projects__intro__details">Raised in the family of a programmer, since childhood I developed passion for digital technologies and beautiful designs. Later my commitment to continuous self-growth and passion to make a meaningful difference in people’s lives led me to work in multiple non-profit organizations and even seek a life of a monk. Now as I broadened my skillset with the newest web technologies I am excited for the new opportunities to make a positive difference in people lives through digital realm. </div>
+          </div>
+        </div>
+        <div className="demo">
+          <div className="demo__title">My Projects</div>
+          <div className="demo__beagiver">
+            <img src={mockup} alt="beagivermockup"/>
+            <div className="demo__beagiver__description"> <span style={{fontWeight:"bold"}}>BeAGiver</span> is a mobile app with the goal to reduce food waste. Over <span style={{fontWeight:"bold"}}> $31 billion </span> dollars worth of food is wasted in Canada each year. This is enough to feed  <span style={{fontWeight:"bold"}}>every Canadian for five month. </span> The app connects charity organizations with grocery stores and restaurants and helps organize pick up of their extra food for further distribution to homeless and people in need.<div style={{fontWeight:"bold", marginTop:"1rem"}}>Technologies used: <span>React</span>,<span>Express</span>,<span>MongoDB</span>,<span>Google API</span></div></div>
+          </div>
+        </div>
     </div>
   );
   }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,8 +7,6 @@ import posed from 'react-pose'
 import Typed from 'react-typed';
 import Stars from '../Stars/Stars'
 import { Link} from 'react-router-dom';
-import {RemoveScroll} from 'react-remove-scroll';
-import {ScrollLocky} from 'react-scroll-locky';
 const Overlay = posed.div({
     start: {backgroundColor: "rgba(0,0,0,0.6)"},
     finish: {backgroundColor: "rgba(255, 150, 124, 0.6)",transition: { duration: 10000 }},
@@ -38,28 +36,28 @@ export default class Main extends React.Component{
     handleImageLoaded2() {
         this.setState({ imageStatus2: true });
     }
-    touchStart(e){
-        this.firstClientX = e.touches[0].clientX;
-        this.firstClientY = e.touches[0].clientY;
-    }
+    // touchStart(e){
+    //     this.firstClientX = e.touches[0].clientX;
+    //     this.firstClientY = e.touches[0].clientY;
+    // }
     
-    preventTouch(e){
-        const minValue = 5; // threshold
+    // preventTouch(e){
+    //     const minValue = 5; // threshold
     
-        this.clientX = e.touches[0].clientX - this.firstClientX;
-        this.clientY = e.touches[0].clientY - this.firstClientY;
+    //     this.clientX = e.touches[0].clientX - this.firstClientX;
+    //     this.clientY = e.touches[0].clientY - this.firstClientY;
     
-        // Vertical scrolling does not work when you start swiping horizontally.
-        if(Math.abs(this.clientX) > minValue){ 
-            e.preventDefault();
-            e.returnValue = false;
-            return false;
-        }
-    }
+    //     // Vertical scrolling does not work when you start swiping horizontally.
+    //     if(Math.abs(this.clientX) > minValue){ 
+    //         e.preventDefault();
+    //         e.returnValue = false;
+    //         return false;
+    //     }
+    // }
     componentDidUpdate(){
         if (this.state.imageStatus1 && this.state.imageStatus2){
-            window.addEventListener('touchstart', this.touchStart);
-            window.addEventListener('touchmove', this.preventTouch, {passive: false});
+            // window.addEventListener('touchstart', this.touchStart);
+            // window.addEventListener('touchmove', this.preventTouch, {passive: false});
             setInterval(()=> {
                 this.setState({ light2: true, isMoving: true})
             }, 100);
@@ -87,13 +85,16 @@ export default class Main extends React.Component{
         
         let {light, light2, isVisible} = this.state;
         return(
-            <ScrollLocky>
+            // <ScrollLocky>
             <div className="main" >   
                 <Overlay2 className="overlay2" pose={light2? 'finish':'start'}></Overlay2>         
                 <Overlay className="overlay" pose={light? 'finish':'start'}></Overlay>
                 <img onLoad={this.handleImageLoaded1.bind(this)} style={topStyle} src={backTop}></img>
                 <Sun isMoving={this.state.isMoving}></Sun>
                 <img onLoad={this.handleImageLoaded2.bind(this)} style={bottomStyle} src={backBottom}></img>
+                <div className="projects">
+                    <h1>Hello</h1>
+                </div>
                 <div className="heading--wrap">
                     <Typed 
                         strings={['Dmitry Goronkov Front/Back-end Web Developer','If you cannot do great things, do small things in a great way']} 
@@ -102,12 +103,26 @@ export default class Main extends React.Component{
                         smartBackspace={true}
                     /><br />
                 </div>
-                    <Buttons className="button--wrap" pose={isVisible? 'visible':'hidden'}>
-                        <Link className="button--link" to="/projects"><button className="button">Projects</button></Link>
-                        <Link className="button--link" to="/profile"><button className="button">Profile</button></Link>
-                    </Buttons>
+                <div class="bird--wrap bird--wrap--one">
+		            <div class="bird bird--one"></div>
+	            </div>
+                <div class="bird--wrap bird--wrap--two">
+		            <div class="bird bird--two"></div>
+	            </div>
+	
+	            <div class="bird--wrap bird--wrap--three">
+		            <div class="bird bird--three"></div>
+	            </div>
+	
+	            <div class="bird--wrap bird--wrap--four">
+		            <div class="bird bird--four"></div>
+	            </div>
+                    {/* <Buttons className="button--wrap" pose={isVisible? 'visible':'hidden'}>
+                        <a className="button--link" to="/projects"><button className="button">Projects</button></a>
+                        <a className="button--link" to="/profile"><button className="button">Profile</button></a>
+                    </Buttons> */}
             </div>
-            </ScrollLocky>
+            // </ScrollLocky>
         )
     }
 }
